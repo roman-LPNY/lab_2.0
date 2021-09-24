ef07b39d89f58513f375c754d0e4474da420be07
Roman Kostiushko <roman.kostiushko.ik.2021@lpnu.ua>
2021-09-24 17:32:05 +0300

    commit 2.1

--- lab_2.0/Source.cpp
+++ lab_2.0/Source.cpp
@@ -10,12 +10,12 @@ int main()
 	double Pi = 4 * atan(1.); // число пі
 	double a;// вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	//double z2; // результат обчислення 2-го виразу
+	double z2; // результат обчислення 2-го виразу
 	cout << "a = "; cin >> a;
 	z1 = (pow(cos((3. / 8 * Pi) - (a / 4)), 2) - pow(cos((11. / 8 * Pi) + (a / 4)), 2));
-	//z2 = ((sqrt(2) / 2) * sin(a / 2));
+	z2 = ((sqrt(2) / 2) * sin(a / 2));
 	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	return 0;
 }
