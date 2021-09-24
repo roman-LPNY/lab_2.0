6081446a0415cd88ab590441c42cf1cfca10d741
Roman Kostiushko <roman.kostiushko.ik.2021@lpnu.ua>
2021-09-24 17:16:30 +0300

    commit 1

--- lab_2.0/Source.cpp
+++ lab_2.0/Source.cpp
@@ -15,7 +15,7 @@ int main()
 	z1 = (pow(cos((3. / 8 * Pi) - (a / 4)), 2) - pow(cos((11. / 8 * Pi) + (a / 4)), 2));
 	z2 = ((sqrt(2) / 2) * sin(a / 2));
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 
 	return 0;
 }
